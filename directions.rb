688c24d7a94cd47612c21de93f13e9ba4a412ca6
michael hassin <hassinmichael@gmail.com>
2016-07-26 12:19:19 -0400

    only geocode start address once

--- directions.rb
+++ directions.rb
@@ -2,6 +2,7 @@ def directions_to address
 
   csv_url = 'https://data.ny.gov/api/views/axi5-gnss/rows.csv?accessType=DOWNLOAD'
   mapzen_key = 'valhalla-ScXGhpE'
+  finish = geocode address
 
   bar_locations = CSV.new(open(csv_url), :headers => :first_row).map do |row|
     [row['Latitude'], row['Longitude']]
@@ -9,7 +10,6 @@ def directions_to address
 
   paths = bar_locations.map do |bar_location|
     start = [bar_location.first, bar_location.last]
-    finish = geocode address
     sleep 0.1
     request = "http://valhalla.mapzen.com/route?json={%22locations%22:[{%22lat%22:#{start[0]},%22lon%22:#{start[1]}},{%22lat%22:#{finish[0]},%22lon%22:#{finish[1]}}],%22costing%22:%22pedestrian%22}&api_key=#{mapzen_key}"
     response = Net::HTTP.get(URI.parse(request))
