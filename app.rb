646b14b6a9be595fc1af02c3cc471467765d8673
michael hassin <hassinmichael@gmail.com>
2016-08-26 00:48:25 -0400

    keepalive and analytics

--- app.rb
+++ app.rb
@@ -21,14 +21,15 @@ class Shame < Sinatra::Base
     directions_to(params[:origin], params[:destination]).to_json
   end
 
-  # seconds_in_three_minutes = 4 * 60
-  # three_minutes = "#{seconds_in_three_minutes}s"
+  seconds_in_three_minutes = 4 * 60
+  three_minutes = "#{seconds_in_three_minutes}s"
 
-  # keep_alive = Rufus::Scheduler.new
-  # keep_alive.every three_minutes, :first_in => 0.1 do
-  #   p Net::HTTP.get(URI.parse(URI.encode("https://walk-of-shame.herokuapp.com/")))
-  #   p Time.now.inspect
-  # end
+  keep_alive = Rufus::Scheduler.new
+  keep_alive.every three_minutes, :first_in => 0.1 do
+    Net::HTTP.get(URI.parse(URI.encode("https://walk-of-shame.herokuapp.com/")))
+    print 'ping '
+    p Time.now.inspect
+  end
 
   run! if app_file == $0
 end
